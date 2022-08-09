1fa19249d4100944b32bbc6b8b531703a48f06d6
LuisHartmannC <lhartmann@uninorte.edu.co>
2022-08-09 14:17:17 -0500

    small fuckup fix.

--- sqlwork.py
+++ sqlwork.py
@@ -3,9 +3,11 @@ from sqlite3 import Error
 
 def sql_connection():
     try:
-        con=sqlite3.connect('luna.db')
+        con=sqlite3.connect('src/sql/luna.db')
         return.con
     except.Error:
         print(Error)
+
+
 
 
