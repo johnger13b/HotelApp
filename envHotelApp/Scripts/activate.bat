29c3ba18b1d3855ee181644a2de4ad9227a55c8f
cwaym <mencarlos@gmail.com>
2022-08-16 22:34:24 -0500

    fix

--- envHotelApp/Scripts/activate.bat
+++ envHotelApp/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\cway\Documents\Programming\Uninorte\Ciclo 3\Hotel Proyecto Entrega\HotelApp\envHotelApp
+set VIRTUAL_ENV=C:\Users\cway\Documents\Programming\Uninorte\Ciclo 3\Proyecto hotel\HotelApp\envHotelApp
 
 if not defined PROMPT set PROMPT=$P$G
 
